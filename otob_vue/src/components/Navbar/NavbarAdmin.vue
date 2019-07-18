e640f614cb87c0ec2f34d3280888e97bb96f657d
NaniHutagaol <nanihutagaol@gmail.com>
2019-07-18 23:38:54 +0700

    merge 2 branch

--- otob_vue/src/components/Navbar/NavbarAdmin.vue
+++ otob_vue/src/components/Navbar/NavbarAdmin.vue
@@ -3,7 +3,11 @@
         <b-navbar-nav>
             <b-nav-item><router-link to="/user">User</router-link></b-nav-item> 
             <b-nav-item><router-link to="/product">Product</router-link></b-nav-item> 
-            <b-nav-item><router-link to="/order">Order</router-link></b-nav-item> 
+            <b-nav-item-dropdown>
+              <template slot="button-content">Order</template>
+              <router-link :to="{name: 'order-approvement'}" tag="b-dropdown-item">Approvement</router-link>
+              <router-link :to="{name: 'orders'}" tag="b-dropdown-item">View All Orders</router-link>
+            </b-nav-item-dropdown>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
