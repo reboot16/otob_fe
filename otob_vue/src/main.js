dcf33081ac453b0ede4f99535be9511a35949180
reboot16 <abdiaruan99@gmail.com>
2019-07-20 22:15:15 +0700

    Add axios interceptor

--- otob_vue/src/main.js
+++ otob_vue/src/main.js
@@ -8,6 +8,18 @@ import './registerServiceWorker'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import BootstrapVue from 'bootstrap-vue'
 import FontAwesome from 'font-awesome/css/font-awesome.css'
+import Axios from 'axios'
+
+Axios.interceptors.request.use(
+    function (config) {
+      config.withCredentials = true
+      config.headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+      return config
+    }
+)
 
 Vue.use(Bootstrap)
 Vue.use(BootstrapVue)
@@ -19,7 +31,6 @@ Vue.use(VueCookies)
 Vue.use(VueLocalStorage) 
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
