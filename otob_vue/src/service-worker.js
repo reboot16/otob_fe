4addea59f2fa0a6afcd9389d4adff262b03fce88
Alexander Nicholas Salim <alexandernsalim@gmail.com>
2019-08-09 10:07:55 +0700

    1st try with PWA

---
+++ otob_vue/src/service-worker.js
@@ -0,0 +1,221 @@
+workbox.setConfig({
+    debug: false
+})
+
+workbox.precaching.precacheAndRoute([
+    {
+        "url": "css/app.b1495c07.css",
+        "revision": "91f1832872ab7d82f677b6f28f6199a9"
+    },
+    {
+        "url": "css/chunk-17aa7e3d.42f476e4.css",
+        "revision": "f9951c4688115fad28ca220117b9fb21"
+    },
+    {
+        "url": "css/chunk-396d1eb0.fa0a8df7.css",
+        "revision": "da6ae9e5e6f7585791f3dc4bab7545da"
+    },
+    {
+        "url": "css/chunk-3a96d575.317f905d.css",
+        "revision": "3bae214786c8063516ea8412ec6667a9"
+    },
+    {
+        "url": "css/chunk-40251826.3eee2b67.css",
+        "revision": "37086ee6c8a4c36e8b6da0e7f1b1cd20"
+    },
+    {
+        "url": "css/chunk-57c4e80b.a393ee25.css",
+        "revision": "ae8ebe1323d66a92f5bc5280c8fafc2d"
+    },
+    {
+        "url": "css/chunk-60a33af6.bdf0c5eb.css",
+        "revision": "fabf0e75e84fa03fdec84b1f7acbdfcb"
+    },
+    {
+        "url": "css/chunk-752c24db.2a34c1f7.css",
+        "revision": "66150f38ac93596d04bbdb833e14037a"
+    },
+    {
+        "url": "css/chunk-7f7cacc2.12380ea5.css",
+        "revision": "03195e661b83fc853933973041a10c42"
+    },
+    {
+        "url": "css/chunk-993d98ee.9d561546.css",
+        "revision": "fbf7e70118272cd4dfffc5cac86bf8ea"
+    },
+    {
+        "url": "css/chunk-b14f263e.f8070613.css",
+        "revision": "0c0f44baa0e5dabd00307bd808eca419"
+    },
+    {
+        "url": "css/chunk-e5096c9c.ee1aaef0.css",
+        "revision": "037d6da4ec3ac3f7598e96b44c9c0b68"
+    },
+    {
+        "url": "css/chunk-vendors.3ec557d0.css",
+        "revision": "3343966746785c66b4735754df0ee115"
+    },
+    {
+        "url": "favicon.ico",
+        "revision": "1ba2ae710d927f13d483fd5d1e548c9b"
+    },
+    {
+        "url": "fonts/fontawesome-webfont.674f50d2.eot",
+        "revision": "674f50d287a8c48dc19ba404d20fe713"
+    },
+    {
+        "url": "fonts/fontawesome-webfont.af7ae505.woff2",
+        "revision": "af7ae505a9eed503f8b8e6982036873e"
+    },
+    {
+        "url": "fonts/fontawesome-webfont.b06871f2.ttf",
+        "revision": "b06871f281fee6b241d60582ae9369b9"
+    },
+    {
+        "url": "fonts/fontawesome-webfont.fee66e71.woff",
+        "revision": "fee66e712a8a08eef5805a46892932ad"
+    },
+    {
+        "url": "img/blibli.8b6831d3.jpeg",
+        "revision": "8b6831d3417fbbe43ad985cb4b531c7a"
+    },
+    {
+        "url": "img/fontawesome-webfont.912ec66d.svg",
+        "revision": "912ec66d7572ff821749319396470bde"
+    },
+    {
+        "url": "img/icons/android-chrome-192x192.png",
+        "revision": "f130a0b70e386170cf6f011c0ca8c4f4"
+    },
+    {
+        "url": "img/icons/android-chrome-512x512.png",
+        "revision": "0ff1bc4d14e5c9abcacba7c600d97814"
+    },
+    {
+        "url": "img/icons/apple-touch-icon-120x120.png",
+        "revision": "936d6e411cabd71f0e627011c3f18fe2"
+    },
+    {
+        "url": "img/icons/apple-touch-icon-152x152.png",
+        "revision": "1a034e64d80905128113e5272a5ab95e"
+    },
+    {
+        "url": "img/icons/apple-touch-icon-180x180.png",
+        "revision": "c43cd371a49ee4ca17ab3a60e72bdd51"
+    },
+    {
+        "url": "img/icons/apple-touch-icon-60x60.png",
+        "revision": "9a2b5c0f19de617685b7b5b42464e7db"
+    },
+    {
+        "url": "img/icons/apple-touch-icon-76x76.png",
+        "revision": "af28d69d59284dd202aa55e57227b11b"
+    },
+    {
+        "url": "img/icons/apple-touch-icon.png",
+        "revision": "66830ea6be8e7e94fb55df9f7b778f2e"
+    },
+    {
+        "url": "img/icons/favicon-16x16.png",
+        "revision": "4bb1a55479d61843b89a2fdafa7849b3"
+    },
+    {
+        "url": "img/icons/favicon-32x32.png",
+        "revision": "98b614336d9a12cb3f7bedb001da6fca"
+    },
+    {
+        "url": "img/icons/msapplication-icon-144x144.png",
+        "revision": "b89032a4a5a1879f30ba05a13947f26f"
+    },
+    {
+        "url": "img/icons/mstile-150x150.png",
+        "revision": "058a3335d15a3eb84e7ae3707ba09620"
+    },
+    {
+        "url": "img/icons/safari-pinned-tab.svg",
+        "revision": "f22d501a35a87d9f21701cb031f6ea17"
+    },
+    {
+        "url": "index.html",
+        "revision": "479e025588ac16de64f4257aca117d3c"
+    },
+    {
+        "url": "js/app.e7835ea7.js",
+        "revision": "411b6af03ded45eb6805adc5975e3fd7"
+    },
+    {
+        "url": "js/chunk-17aa7e3d.835a0b26.js",
+        "revision": "3d7c0e5ce1ef08bf19be5be0789dce45"
+    },
+    {
+        "url": "js/chunk-396d1eb0.d36bc4fd.js",
+        "revision": "086228a4e88bcd77e4156607233d9e35"
+    },
+    {
+        "url": "js/chunk-3a96d575.bc8df1dd.js",
+        "revision": "7741b82526e0456495b2d3bcd3042171"
+    },
+    {
+        "url": "js/chunk-40251826.0faab44a.js",
+        "revision": "8a34b734d3986675480a1847b845f4a1"
+    },
+    {
+        "url": "js/chunk-57c4e80b.240009ab.js",
+        "revision": "55f91279c3c3980350ade59b0f0456ee"
+    },
+    {
+        "url": "js/chunk-60a33af6.15bd2ed7.js",
+        "revision": "d3b496e7c19b853c585c3d0c45411d90"
+    },
+    {
+        "url": "js/chunk-752c24db.0195b3d8.js",
+        "revision": "46b8da00c0138f20f89d37d24d4ae21e"
+    },
+    {
+        "url": "js/chunk-7f7cacc2.e85ce521.js",
+        "revision": "2a904daf7dfed7896ce32ce93f267d85"
+    },
+    {
+        "url": "js/chunk-993d98ee.6218d25b.js",
+        "revision": "e9468a48af2f10e6d885055e139636ee"
+    },
+    {
+        "url": "js/chunk-b14f263e.a23728ed.js",
+        "revision": "328d7b10e66b869a434cd37108d8f1c5"
+    },
+    {
+        "url": "js/chunk-e5096c9c.62ecef9f.js",
+        "revision": "fca163287a0423da343db8493d1a4a8c"
+    },
+    {
+        "url": "js/chunk-vendors.2645f9f4.js",
+        "revision": "8043a43363c1212caddf343dac32376f"
+    },
+    {
+        "url": "js/pdfjsWorker.c097b7e7.js",
+        "revision": "6fbe76904c7e365c017db265c7fc6c7f"
+    },
+    {
+        "url": "manifest.json",
+        "revision": "539bbb8360fffca6d5d276b43c959eab"
+    },
+    {
+        "url": "precache-manifest.7dbbc377e31029ca03d41c7b97e5666f.js",
+        "revision": "7dbbc377e31029ca03d41c7b97e5666f"
+    },
+    {
+        "url": "robots.txt",
+        "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca"
+    },
+    {
+        "url": "service-worker.js",
+        "revision": "de1c69ae44737a09eadc336856754fdf"
+    }
+])
+
+workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'blizaar-core-cache'
+    })
+)
