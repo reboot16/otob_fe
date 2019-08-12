6ee5e7871c11c0b22d2b8be027a49a270fe1da1b
NaniHutagaol <nanihutagaol@gmail.com>
2019-08-12 13:28:36 +0700

    modified some item on components

--- otob_vue/src/components/TableCart/index.vue
+++ otob_vue/src/components/TableCart/index.vue
@@ -1,18 +1,18 @@
 <template> 
   <div name="TableCart">
-
-    <div class="tableContainer">
-      <table width="100%" class="table table-scroll small-first-col" style="border-radius: 1em; ">
-        <tbody class="scrollContentCart">
-          <tr ref="listItemCart" class="col-sm-12" v-if="listItemCart.length != 0"
+      <table width="100%" class="table">
+        <tbody>
+          <tr  class="col-sm-12"
               v-for="(product, index) in listItemCart" :key="index" >
             <td class="col-sm-7">{{ product.productName }}</td>
-            <td class="col-sm-2 color-orange cart-price" width="25%">Rp {{ product.productPrice }}</td>
+            <td class="col-sm-2">{{ getFormattedCurrency (product.productPrice) }}</td>
             <td class="col-sm-2" style="text-align: center;">
-              <div class="quantity-toggle">
-                <button @click="decrement(product, index)" class="btn btn-gray btn-left">&mdash;</button>
-                <input type="text" :value="product.qty" readonly class="input-gray">
-                <button @click="increment(product, index)" class="btn btn-gray btn-right">&#xff0b;</button>
+              <div  style="display: flex" class="row">
+                <div class=" qty">
+                  <button @click="decrement(product, index)" class="btn btn-gray btn-left">&mdash;</button>
+                  <input type="text" :value="product.qty" readonly class="input-gray">
+                  <button @click="increment(product, index)"class="btn btn-gray btn-right">&#xff0b;</button>
+                </div>
               </div>
             </td>
             <td class="col-sm-1" style="text-align: right;">
@@ -23,18 +23,13 @@
               </button>
             </td>
           </tr>
-
-          <tr class="col-sm-12" v-else>
-            <td width="4%" class="empty-cart" style="text-align: center"><h5>Empty cart :(</h5></td>
-          </tr>
         </tbody>
 
       </table>
-    </div>
     <div class="row" style="margin-bottom: 0.5em">
       <div class="col-sm-10" style="text-align:right">
         <div style="font-size: 13px">Total Belanja <b> ({{ getFormattedCurrency(countOfItem) }}) </b></div>
-        <span class="color-orange checkout-price"> {{ getFormattedCurrency(sumOfPrice) }}</span>
+        <span class="color-orange checkout-price"> {{ sumOfPrice }}</span>
       </div>
       <div class="col-sm-2" style="text-align:right" >
         <button class="btn btn-orange" @click="onOrder" :disabled="bookDisable(countOfItem)">Order Now</button>
